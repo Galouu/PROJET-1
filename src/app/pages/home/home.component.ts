0cc04a8c684bacb7e79d1f9bb26380997a6b792d
Galaad Michaud <gannette971@hotmail.fr>
2024-09-13 16:30:00 +0200

    Integrate ng2-charts for Olympic data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,39 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
 
+  public barChartData: ChartData<'bar'> | undefined;
+  public barChartOptions: ChartOptions<'bar'> = {
+    responsive: true,
+  };
+
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.subscribe((data) => {
+      if (data) {
+        this.prepareChartData(data);
+      }
+    });
+  }
+
+  private prepareChartData(data: any) {
+    const countries = data.map((olympic: any) => olympic.country);
+    const medals = data.map((olympic: any) =>
+      olympic.participations.reduce((acc: number, p: any) => acc + p.medalsCount, 0)
+    );
+
+    this.barChartData = {
+      labels: countries,
+      datasets: [
+        {
+          label: 'Total Medals',
+          data: medals,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+        },
+      ],
+    };
   }
 }
